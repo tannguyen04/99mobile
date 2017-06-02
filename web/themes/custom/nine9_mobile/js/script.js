4032170846bf1a619c16f7785c33f8bbacb94d19
Tan Nguyen <tan.nguyen@ffwagency.com>
2017-06-02 10:23:18 +0700

    Check slide

--- web/themes/custom/nine9_mobile/js/script.js
+++ web/themes/custom/nine9_mobile/js/script.js
@@ -13,33 +13,33 @@
       tablet = "(min-width:48em)"; // 768px.
       // Add  functionality here.
       $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+        // slidesToShow: 3,
+        // slidesToScroll: 1,
+        // responsive: [
+        //   {
+        //     breakpoint: 1024,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 3,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 2,
+        //       slidesToScroll: 2
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1
+        //     }
+        //   }
+        // ]
       });
     }
   };
@@ -97,7 +97,7 @@
       $('.js-show-gallery').on('touchstart click', function (e) {
         $('.product__slide').toggleClass('is-active');
       });
-      
+
       $('.js-close i').on('touchstart click', function (e) {
         $('.product__slide').toggleClass('is-active');
       });
