528accce27fc27615ca3703d63b838edfb20a1c7
Tan Nguyen <tan.nguyen@ffwagency.com>
2017-08-04 21:26:30 +0700

    update

--- web/themes/custom/nine9_mobile/js/script.js
+++ web/themes/custom/nine9_mobile/js/script.js
@@ -60,7 +60,23 @@
         infinite: true,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
     }
   };
